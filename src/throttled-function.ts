58a053a4babe0c8bc65fbd03870ba23ab802fcc7
Ben Meyrick <bameyrick@gmail.com>
2021-06-21 16:13:45 +0100

    Improve comments

--- src/throttled-function.ts
+++ src/throttled-function.ts
@@ -3,10 +3,7 @@ export interface ThrottledFunction<T extends (...args: any) => any> {
    * Call the original function, but applying the throttle rules.
    *
    * If the throttled function can be run immediately, this calls it and returns its return
-   * value.
-   *
-   * Otherwise, it returns the return value of the last invocation, or undefined if the throttled
-   * function was not invoked yet.
+   * value, otherwise, it returns the return value of the last invocation.
    */
   (...args: Parameters<T>): ReturnType<T>;
 }
