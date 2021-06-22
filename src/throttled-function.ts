c5ec921036dd1ae839c0651cf042073ed7f3be44
Ben Meyrick <bameyrick@gmail.com>
2021-06-22 11:43:11 +0100

    Get tests reporting coverage, fix git hooks, prepare for travis testing/publishing

--- src/throttled-function.ts
+++ src/throttled-function.ts
@@ -1,9 +1,7 @@
-export interface ThrottledFunction<T extends (...args: any) => any> {
-  /**
-   * Call the original function, but applying the throttle rules.
-   *
-   * If the throttled function can be run immediately, this calls it and returns its return
-   * value, otherwise, it returns the return value of the last invocation.
-   */
-  (...args: Parameters<T>): ReturnType<T>;
-}
+/**
+ * Call the original function, but applying the throttle rules.
+ *
+ * If the throttled function can be run immediately, this calls it and returns its return
+ * value, otherwise, it returns the return value of the last invocation.
+ */
+export type ThrottledFunction<T extends (...args: any) => any> = (...args: Parameters<T>) => ReturnType<T>;
